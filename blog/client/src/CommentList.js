f73a4eadf10cb4bc28868ee75fd690062d28732b
Thomas Ngo <tngo0508@gmail.com>
2023-04-21 14:39:36 -0700

    implement frontend

---
+++ blog/client/src/CommentList.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const CommentList = ({ postId }) => {
+  const [comments, setComment] = useState([]);
+  const fetchData = async () => {
+    const res = await axios.get(
+      `http://localhost:4001/posts/${postId}/comments`
+    );
+    setComment(res.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const renderedComments = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
+
+export default CommentList;
